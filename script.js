94e4678a3f1f0dfd9bb7106d6b58042dae08dc3f
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 15:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function createTable() {
     //Write your code here
-  
+	const table= document.querySelector("#myTable");
+	table.innerHTML= "";
+	
+	const row = prompt("Input number of rows");
+	const col = prompt("Input number of columns");
+	
+	for(let i=0; i<row; i++) {
+		const tr= document.createElement("tr");
+		for(let j=0; j<col; j++){
+			const td= document.createElement("td");
+			td.innerText= `Row-${i} Column-${j}`;
+			tr.append(td);
+		}
+		table.append(tr);
+	}
 }
+
+
+
+
+
+
